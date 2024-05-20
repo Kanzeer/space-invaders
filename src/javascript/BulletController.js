892719f602ffd78e6e8a272cf00e747111758b58
Kanzeer <miguelroaamaral@gmail.com>
2024-05-20 14:52:33 -0400

    add draw function to draw the bullets on the screen

--- src/javascript/BulletController.js
+++ src/javascript/BulletController.js
@@ -1,5 +1,5 @@
 class BulletController {
-    bullet = [];
+    bullets = [];
     timeTillNextBulletAllowed = 0;
 
     constructor(canvas, maxBulletAtTime, bulletColor, soundEnable) {
@@ -10,5 +10,17 @@ class BulletController {
         
         this.bulletSound = new Audio("src/assets/sound/shoot.wav");
         this.bulletSound.volume = 0.1;
-    } 
+    }
+
+    draw(ctx) {
+        this.bullets = this.bullets.filter(
+            (bullet) => bullet.y + bullet.width > 0 && bullet.y < this.canvas.height);
+
+         this.bullets.forEach((bullet ) => bullet.draw(ctx));
+        
+        if(this.timeTillNextBulletAllowed > 0) {
+            this.timeTillNextBulletAllowed--;
+        }
+
+    }
 }
