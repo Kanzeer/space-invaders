a66a56267ef9af1ed920af39fd5ef545c09d5837
Kanzeer <miguelroaamaral@gmail.com>
2024-05-27 16:24:45 -0400

    add collideWith function in EnemyController class

--- src/javascript/EnemyController.js
+++ src/javascript/EnemyController.js
@@ -100,4 +100,8 @@ export default class EnemyController {
                 });
             });
         }
+
+        collideWith(sprite) {
+            return this.enemyRows.flat().some((enemy) => enemy.collideWith(sprite));
+        }
 }
