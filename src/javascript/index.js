7574ec2ed872f87071de77713f157ab4187f7705
Kanzeer <miguelroaamaral@gmail.com>
2024-05-29 14:31:11 -0400

    add enemy and player control

--- src/javascript/index.js
+++ src/javascript/index.js
@@ -1,6 +1,7 @@
 import EnemyController from './EnemyController';
 import BulletController from './BulletController';
 import Player from './Player';
+import EnemyController from './EnemyController';
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 
@@ -9,3 +10,12 @@ canvas.height = 600;
 
 const background = new Image();
 background.src = "./src/assets/images/space.png";
+
+const playerBulletController = new BulletController(canvas, 10, "whuthite", true);
+const enemyBulletController = new EnemyController(canvas, 4,, "red", false);
+
+const EnemyController = new EnemyController(
+    canvas,
+    enemyBulletController,
+    enemyBulletController
+);
